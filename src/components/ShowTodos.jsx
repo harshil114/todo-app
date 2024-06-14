e2fd963758e8bbb04dcdd760b97c18a97a0f3cd1
harshil114 <harshilvispute114@gmail.com>
2024-06-14 16:03:01 +0530

    style: ShowTodos.jsx

--- src/components/ShowTodos.jsx
+++ src/components/ShowTodos.jsx
@@ -2,15 +2,11 @@ import React from "react";
 
 function ShowTodos() {
   return (
-    <div>
-      <div>
-        <h2>Your Todos</h2>
-        <div className="todos">
-          <div className="todo">
-            <div>Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
-          </div>
-        </div>
-      </div>
+    <div className="showTodo-container">      
+        <h2 className="showTodo-heading font-bold mb-4 text-xl">Your Todos</h2>        
+          <div className="todos">
+            <div className="todo font-medium">Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
+          </div>              
     </div>
   );
 }
