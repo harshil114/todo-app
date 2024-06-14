8fec8f907a18c8cbcd1cdcced75ce7025d282f52
harshil114 <harshilvispute114@gmail.com>
2024-06-14 15:58:00 +0530

    style: AddTodo.jsx

--- src/components/AddTodos.jsx
+++ src/components/AddTodos.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function AddTodos() {
   return (
-    <div>
-      <h2>Add a Todo</h2>
-      <input type="text" className='' />
-      <button className="button">Save</button>      
+    <div className="input-container">
+      <h2 className="input-heading font-bold mb-4 text-lg">Add a Todo</h2>
+      <div className="input-body flex gap-2 mb-4">
+        <input type="text" className="input w-full rounded-md py-1" />
+        <button className="button bg-slate-900 text-white py-1 px-3 rounded-md hover:bg-slate-950 active:bg-slate-900">
+          Save
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddTodos
+export default AddTodos;
